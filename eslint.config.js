6f5250f19a3ef130515375f43a08d4c1f60b2dfe
FTWinston <ftwinston@gmail.com>
2025-09-05 21:53:25 +0100

    Add storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import reactPlugin from "eslint-plugin-react";
@@ -41,4 +44,5 @@ export default [
         },
     },
     prettierConfig,
+    ...storybook.configs["flat/recommended"],
 ];
