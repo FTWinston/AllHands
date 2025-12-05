3b23a75971a3633b59dfd20a68977a1b8834e615
FTWinston <ftwinston@gmail.com>
2025-12-05 00:31:30 +0000

    Resolve some startup issues

--- packages/game-ui/vite.config.ts
+++ packages/game-ui/vite.config.ts
@@ -4,6 +4,7 @@ import * as path from 'path';
 
 export default defineConfig({
     ...baseViteConfig,
+    base: './', // Use relative paths for Electron compatibility
     resolve: {
         alias: {
             ...baseViteConfig.resolve?.alias,
