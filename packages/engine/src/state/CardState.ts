f7a208024abeec1903c82257e4dcee175fc75466
FTWinston <ftwinston@gmail.com>
2026-04-17 11:50:45 +0100

    add modifyParameter method to CardState

--- packages/engine/src/state/CardState.ts
+++ packages/engine/src/state/CardState.ts
@@ -1,6 +1,7 @@
 import { MapSchema, Schema, type } from '@colyseus/schema';
 import { CardInstance } from 'common-data/features/cards/types/CardInstance';
 import { CardType } from 'common-data/features/cards/utils/cardDefinitions';
+import { getCardDefinition } from 'src/cards/getEngineCardDefinition';
 
 export class CardState extends Schema implements CardInstance {
     constructor(id: number, type: CardType) {
@@ -12,4 +13,25 @@ export class CardState extends Schema implements CardInstance {
     @type('number') id: number;
     @type('string') type: CardType;
     @type({ map: 'number' }) modifiers = new MapSchema<number>();
+
+    hasParameter(parameter: string): boolean {
+        const definition = getCardDefinition(this.type);
+
+        return definition.parameters?.has(parameter) ?? false;
+    }
+
+    modifyParameter(parameter: string, adjustment: number) {
+        if (!this.hasParameter(parameter)) {
+            return;
+        }
+
+        const current = this.modifiers.get(parameter) || 0;
+        const adjusted = current + adjustment;
+        
+        if (adjusted === 0) {
+            this.modifiers.delete(parameter);
+        } else {
+            this.modifiers.set(parameter, adjusted);
+        }
+    }
 }
