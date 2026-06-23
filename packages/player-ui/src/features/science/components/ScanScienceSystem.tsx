bd460e7b44d580b379601ec4e3ba4d753a4333d7
FTWinston <ftwinston@gmail.com>
2026-06-23 22:21:57 +0100

    Improve sensors display on science scan

--- packages/player-ui/src/features/science/components/ScanScienceSystem.tsx
+++ packages/player-ui/src/features/science/components/ScanScienceSystem.tsx
@@ -1,9 +1,11 @@
-import { Snapshot } from '@colyseus/react';
+import { IArray, Snapshot } from '@colyseus/react';
+import { ShipSystem } from 'common-data/features/ships/types/ShipSystem';
 import { ScannedScienceInfo } from 'common-data/features/space/types/GameObjectInfo';
 import { InfoPopup } from 'common-ui/components/InfoPopup';
 import { Card } from 'common-ui/features/cards/components/Card';
 import { getCardDefinition } from 'common-ui/features/cards/utils/getUiCardDefinition';
 import { classNames } from 'common-ui/utils/classNames';
+import React from 'react';
 import { ScanBase } from './ScanBase';
 import styles from './ScanScienceSystem.module.css';
 
@@ -32,22 +34,36 @@ const DeflectorSlot = (props: Snapshot<ScannedScienceInfo>) => {
     }
 };
 
-export const ScanScienceSystem = (props: Props) => {
-    const scanTargets = !props.scanSystems || props.scanSystems.length === 0
-        ? <div className={styles.notScanning}>(Not scanning your ship)</div>
-        : (
-            <div className={styles.scanTargetList}>
-                Your
-                {' '}
-                {props.scanSystems.join(', ')}
-            </div>
-        );
+const ScanTargetList = ({ scanSystems }: { scanSystems: IArray<ShipSystem> }) => {
+    if (!scanSystems || scanSystems.length === 0) {
+        return <div className={styles.notScanning}>(Not scanning your ship)</div>;
+    }
 
+    const nodes: React.ReactNode[] = [];
+    scanSystems.forEach((system, i) => {
+        if (i > 0) {
+            nodes.push(i === scanSystems.length - 1 ? ' & ' : ', ');
+        }
+        nodes.push(<span key={i} className={styles.systemName}>{system}</span>);
+    });
+
+    return (
+        <div className={classNames(styles.scanTargetList, styles[`scanTargetList_num${scanSystems.length}`])}>
+            Scanning your
+            {' '}
+            {nodes}
+            {scanSystems.length === 1 ? ' system' : ' systems'}
+            .
+        </div>
+    );
+};
+
+export const ScanScienceSystem = (props: Props) => {
     return (
         <ScanBase className={styles.root} expanded>
             <div className={styles.scanTargets}>
-                <div className={styles.itemLabel}>Scanning</div>
-                {scanTargets}
+                <div className={styles.itemLabel}>Sensors</div>
+                <ScanTargetList scanSystems={props.scanSystems} />
             </div>
             <DeflectorSlot {...props} />
         </ScanBase>
